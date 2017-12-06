343ae5a5b30a16a1908bb05e243801c7eff03252
Wikus Pretorious <WikusP@datacom.co.nz>
2017-12-06 13:58:14 +1300

    Add project files.

---
+++ UnitTestProject1/UnitTestOktaCoreClient.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Okta.Core;
+using Okta.Core.Clients;
+using Okta.Core.Models;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// This UnitTest is implemented using Okta.Core.Client v0.3.3
+    /// </summary>
+    [TestClass]
+    public class UnitTestOktaCoreClient
+    {
+        readonly string _token = "00bPbh-EP6gcqj2bNTtSfeu_GOzamGh5aph4tJCkWl";
+        readonly string _orgUrl = "https://dev-399435.oktapreview.com";
+
+        /// <summary>
+        /// Create a (random) Profile and attempt to set a custom attribute value (for attribute "CrmId").
+        /// </summary>
+        [TestMethod]
+        public void CreateUserAndSetCustomAttributeTest()
+        {
+            var usersClient = new UsersClient(_token, new Uri(_orgUrl));
+
+            var identifier = DateTime.Now.Ticks.ToString();
+            var email = $"Test_{identifier}@test.com";
+            var user = new User(email, email, $"OktaCoreClient_FN_{identifier}", $"OktaCoreClient_LN{identifier}");
+
+            // Create the user
+            user = usersClient.Add(user);
+
+            //Test Retrieve
+            var userData = usersClient.Get(email);
+
+            userData.SetProperty("CrmId", identifier);
+            userData = usersClient.Update(userData);
+
+            Assert.IsNotNull(usersClient);
+        }
+
+        [TestMethod]
+        public void RetrieveProfilesTest()
+        {
+            var usersClient = new UsersClient(_token, new Uri(_orgUrl));
+
+            Uri nextPage = null;
+            PagedResults<User> users;
+
+            var foundUsers = false;
+            do
+            {
+                users = usersClient.GetList(pageSize: 100, nextPage: nextPage);
+
+                foundUsers = (users.Results.Count > 0) || foundUsers;
+
+                foreach (var user in users.Results)
+                {
+                    // Do something with each user
+                }
+                nextPage = users.NextPage;
+            }
+            while (!users.IsLastPage);
+
+            Assert.IsTrue(foundUsers);
+        }
+
+
+  
+    }
+}
---
+++ UnitTestProject2/UnitTestOktaSdk.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Okta.Sdk;
+using Okta.Sdk.Configuration;
+
+namespace UnitTestProject2
+{
+    /// <summary>
+    /// This UnitTest is implemented using Okta.Sdk (PreRelease) 1.0.0-alpha4
+    /// </summary>
+    [TestClass]
+    public class UnitTestOktaSdk
+    {
+        static string _token = "00bPbh-EP6gcqj2bNTtSfeu_GOzamGh5aph4tJCkWl";
+        static string _orgUrl = "https://dev-399435.oktapreview.com";
+
+        /// <summary>
+        /// Create Profile with 'Nickname' and 'CrmId' (Custom Attribute) containing a value.
+        /// Failure.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task CreateProfileWithNickNameAndCustomAttribute()
+        {
+            var client = new OktaClient(
+                new OktaClientConfiguration
+                {
+                    OrgUrl = _orgUrl,
+                    Token = _token
+                });
+
+            var identifier = DateTime.Now.Ticks.ToString();
+            var email = $"Test_{identifier}@test.com";
+
+            var user = await client.Users.CreateUserAsync(
+                // User with password
+                new CreateUserWithPasswordOptions
+                {
+                    // User profile object
+                    Profile = new UserProfile
+                    {
+                        FirstName = $"OktaSdk_FN_{identifier}",
+                        LastName = $"OktaSdk_LN_{identifier}",
+                        Email = email,
+                        Login = email,
+                        ["nickName"] = $"OnCreate_{identifier}",
+                        ["CrmId"] = $"{identifier}"
+                    },
+                    Password = "D1sturB1ng!",
+                    Activate = false,
+                });
+        }
+
+        /// <summary>
+        /// Create Profile with 'Nickname' and 'CrmId' (Custom Attribute) containing a value.
+        /// Update Profile.'Nickname' after create.
+        /// Update Profile.'CrmId' after create.
+        /// Failure.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task CreateProfileUpdateNickNameAndCustomAttribute()
+        {
+            var client = new OktaClient(
+                new OktaClientConfiguration
+                {
+                    OrgUrl = _orgUrl,
+                    Token = _token
+                });
+
+            var identifier = DateTime.Now.Ticks.ToString();
+            var email = $"Test_{identifier}@test.com";
+
+            var user = await client.Users.CreateUserAsync(
+                // User with password
+                new CreateUserWithPasswordOptions
+                {
+                    // User profile object
+                    Profile = new UserProfile
+                    {
+                        FirstName = $"OktaSdk_FN_{identifier}",
+                        LastName = $"OktaSdk_LN_{identifier}",
+                        Email = email,
+                        Login = email,
+                        ["nickName"] = $"OnCreate_{identifier}",
+                        ["CrmId"] = $"{identifier}"
+                    },
+                    Password = "D1sturB1ng!",
+                    Activate = false,
+                });
+
+            user.Profile["nickName"] = $"OnUpdate_{identifier}";
+            user.Profile["CrmId"] = $"CrmId_{identifier}";
+            user = await user.UpdateAsync();
+        }
+
+        /// <summary>
+        /// Create Profile with 'Nickname' containing a value.
+        /// Successful.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task CreateProfileWithNickName()
+        {
+            var client = new OktaClient(
+                new OktaClientConfiguration
+                {
+                    OrgUrl = _orgUrl,
+                    Token = _token
+                });
+
+
+            var identifier = DateTime.Now.Ticks.ToString();
+            var email = $"Test_{identifier}@test.com";
+
+            var user = await client.Users.CreateUserAsync(
+                // User with password
+                new CreateUserWithPasswordOptions
+                {
+                    // User profile object
+                    Profile = new UserProfile
+                    {
+                        FirstName = $"OktaSdk_FN_{identifier}",
+                        LastName = $"OktaSdk_LN_{identifier}",
+                        Email = email,
+                        Login = email,
+                        ["nickName"] = $"OnCreate_{identifier}"
+                        //["CrmId"] = "13321"
+                    },
+                    Password = "D1sturB1ng!",
+                    Activate = false,
+                });
+        }
+
+        /// <summary>
+        /// Create Profile with 'Nickname' containing a value.
+        /// Update Profile.'Nickname' after create.
+        /// Successful.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task CreateProfileUpdateNickName()
+        {
+            var client = new OktaClient(
+                new OktaClientConfiguration
+                {
+                    OrgUrl = _orgUrl,
+                    Token = _token
+                });
+
+            var identifier = DateTime.Now.Ticks.ToString();
+            var email = $"Test_{identifier}@test.com";
+
+            var user = await client.Users.CreateUserAsync(
+                // User with password
+                new CreateUserWithPasswordOptions
+                {
+                    // User profile object
+                    Profile = new UserProfile
+                    {
+                        FirstName = $"OktaSdk_FN_{identifier}",
+                        LastName = $"OktaSdk_LN_{identifier}",
+                        Email = email,
+                        Login = email,
+                        ["nickName"] = $"OnCreate_{identifier}"
+                        //["CrmId"] = "13321"
+                    },
+                    Password = "D1sturB1ng!",
+                    Activate = false,
+                });
+
+
+            user.Profile["nickName"] = $"OnUpdate_{identifier}";
+            user = await user.UpdateAsync();
+        }
+
+    }
+}
